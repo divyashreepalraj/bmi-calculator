4b2590bd06fd6bbfac2425455dac67ba995b2634
Divya Shree Palraj <divyashreepalraj2001@gmail.com>
2026-09-20 22:37:42 +0530

    Calculator UI Done

---
+++ custom.js
@@ -0,0 +1,37 @@
+
+const submit = document.getElementById("submit");
+
+submit.addEventListener("click", function (e) {
+
+    e.preventDefault();
+
+    const height = document.getElementById("heightField").value;
+    const weight = document.getElementById("weightField").value;
+
+    const heightInMeters = height / 100;
+
+    const bmi = weight / (heightInMeters * heightInMeters);
+
+    document.getElementById("bmiscore").innerText = bmi.toFixed(1);
+
+
+});
+
+
+
+
+
+
+let bmiScore = document.getElementById("bmiscore").innerText;
+let weightIndicator = ""
+ if (bmiScore <= 18.5) {
+    weightIndicator = document.getElementById("weightIndicator").innerHTML = '<span></span> Underweight';
+ } else if (bmiScore <= 24.9) {
+    weightIndicator = document.getElementById("weightIndicator").innerHTML = "<span></span> Normal Weight";
+ } else if (bmiScore <= 29.9) {
+    weightIndicator = document.getElementById("weightIndicator").innerHTML = "<span></span> Overweight";
+ } else {
+    weightIndicator = document.getElementById("weightIndicator").innerHTML = "<span></span> Obesity";
+ }
+
+  
